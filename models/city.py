cbe45ff5bbbf65fc937c41384e388498df04885a
kevkiiru <kevkiiru6@gmail.com>
2022-10-29 16:52:22 +0300

    city py file

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for class city"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """class representing a city"""
+    state_id = ""
+    name = ""
